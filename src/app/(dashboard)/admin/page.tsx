e4cd54ef361b897b78ae0c96225cd8558697afcc
John Nelson <jrnelson75032@gmail.com>
2024-12-11 07:06:20 -0600

    Completes attendance and finance charts

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,4 +1,6 @@
+import AttendanceChart from "@/components/AttendanceChart"
 import CountChart from "@/components/CountChart"
+import FinanceChart from "@/components/FinanceChart"
 import UserCard from "@/components/UserCard"
 
 const AdminPage = () => {
@@ -19,6 +21,14 @@ const AdminPage = () => {
           <div className="w-full lg:w-1/3 h-[450px]">
             <CountChart />
           </div>
+          {/* ATTENDANCE CHART*/}
+          <div className="w-full lg:2/3 h-[450px]">
+            <AttendanceChart />
+          </div>
+        </div>
+        {/* FINANCE CHART*/}
+        <div className="w-full h-[500px]">
+          <FinanceChart />
         </div>
       </div>
       {/* RIGHT */}
