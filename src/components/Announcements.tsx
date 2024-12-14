1efdeaec117ae30c821535f5e18010e15a5a4ce2
John Nelson <jrnelson75032@gmail.com>
2024-12-14 17:00:24 -0600

    Completes Admin page

---
+++ src/components/Announcements.tsx
@@ -0,0 +1,43 @@
+const Announcements = () => {
+  return (
+    <div className='bg-white p-4 rounded-md'>
+        <div className="flex items-center justify-between">
+            <h1 className="text-xl font-semibold">Announcements</h1>
+            <span className="text-xs text-gray-400">View All</span>
+        </div>
+        <div className="flex flex-col gap-4 mt-4">
+            <div className="bg-lamaSkyLight rounded-md p-4">
+                <div className="flex items-center justify-between">
+                    <h2 className="font-medium">Lorem ipsum dolor sit.</h2>
+                    <span className="text-xs text-gray-400 bg-white rounded-md px-1 py-1">
+                        2024-12-13
+                    </span>
+                </div>
+                <p className="text-gray-400 mt-1 text-sm">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis dicta fugit assumenda.</p>
+            </div>
+
+            <div className="bg-lamaPurpleLight rounded-md p-4">
+                <div className="flex items-center justify-between">
+                    <h2 className="font-medium">Lorem ipsum dolor sit.</h2>
+                    <span className="text-xs text-gray-400 bg-white rounded-md px-1 py-1">
+                        2024-12-13
+                    </span>
+                </div>
+                <p className="text-gray-400 mt-1 text-sm">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis dicta fugit assumenda.</p>
+            </div>
+
+            <div className="bg-lamaYellowLight rounded-md p-4">
+                <div className="flex items-center justify-between">
+                    <h2 className="font-medium">Lorem ipsum dolor sit.</h2>
+                    <span className="text-xs text-gray-400 bg-white rounded-md px-1 py-1">
+                        2024-12-13
+                    </span>
+                </div>
+                <p className="text-gray-400 mt-1 text-sm">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis dicta fugit assumenda.</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Announcements
